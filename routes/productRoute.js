d48834ef168fb5e56f6d165a85fa805762c21764
Sumit Pokhriyal <sumitstan1998@gmail.com>
2023-02-27 11:20:54 +0530

    sort functionality

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,6 @@
 const {ProductModel} = require("../models/ProductsModel");
-const {verifyTokenAndAdmin}=require("../middlewares/VerifyTokenAndAdmin")
+const {verifyTokenAndAdmin}=require("../middlewares/VerifyTokenAndAdmin");
+const { hash } = require("bcrypt");
 const ProductsRoute = require("express").Router();
 
 //CREATE ,  Only Admin Authorised middleware(verifyTokenAndAdmin)
@@ -59,84 +60,70 @@ ProductsRoute.get("/:id", async (req, res) => {
 
 //GET ALL ,Product, Anyone can access
 
-// ProductsRoute.get("/", async (req, res) => {
-//   const qNew = req.query.new;
-//   const qCategory = req.query.category;
-//   console.log({qCategory})
-//   try {
-//     let products;
-
-//     if (qNew) {
-//       products = await ProductModel.find().sort({ createdAt: -1 }).limit(10);
-//     } else if (qCategory) {
-//       products = await ProductModel.find({
-//         categories:{
-//           $in:[qCategory],
-//         },
-//       });
-//     } else {
-//       products = await ProductModel.find();
-//     }
-
-//     res.status(200).send(products);
-//   } catch (err) {
-//     res.status(500).send(err);
-//   }
-// });
 ProductsRoute.get("/", async (req, res) => {
-  const query=req.body
-  const qNew = req.query.new;
-  const qCategory = req.query.categories;
-  console.log({qCategory})
-  const price=req.query.price
-  const color=req.query.color
-  const sort=req.query.order
+  const query = req.query;
+
   try {
-    let products;
-   
-    if(price!==undefined &&color!==undefined && qCategory!==undefined){
-      products=await ProductModel.find({$and:[{categories:{$in:[qCategory]}},{price:{$lte:price}},{color:{$regex:`${color}`,$options:"i"}}]})
-    }
-    else if(qCategory!==undefined && price!==undefined){
-      products=await ProductModel.find({$and:[{categories:{$in:[qCategory]}},{price:{$lte:price}}]})
-    }
-    else if(qCategory!==undefined&&color!==undefined){
-      products=await ProductModel.find({$and:[{categories:{$in:[qCategory]}},{color:{$regex:`${color}`,$options:"i"}}]})
-    }
-    else if(color!==undefined && price!==undefined){
-      products=await ProductModel.find({$and:[{price:{$lte:price}},{color:{$regex:`${color}`,$options:"i"}}]})
-    }
-    else {
-      if(qCategory!==undefined){
-        products = await ProductModel.find({
-            categories:{
-              $in:[qCategory],
-            },
-        });
-      }
-      else if(color!==undefined){
-        products=await ProductModel.find({color})
-      }
-      else if(price!==undefined){
-        products=await ProductModel.find({price})
-      }
-      else {
-        products=await ProductModel.find()
-      }
-    }
-    
-   if(sort!==undefined){
-    if(sort=='asc'){
-      products.sort((a,b)=>a.price-b-price)
-    }
-    else{
-      products.sort((a,b)=>b.price-a.price)
-    }
-   }
-    products.length>0?res.status(200).send(products):res.status(200).send("There is no products")
+   let products=await ProductModel.find(query);
+   res.status(200).send(products);
   } catch (err) {
     res.status(500).send(err);
   }
 });
+// ProductsRoute.get("/", async (req, res) => {
+//   const query=req.body
+//   const qNew = req.query.new;
+//   const qCategory = req.query.categories;
+//   console.log({qCategory})
+//   const price=req.query.price
+//   const color=req.query.color
+//   const sort=req.query.order
+//   try {
+//     let products;
+   
+//     if(price!==undefined &&color!==undefined && qCategory!==undefined){
+//       products=await ProductModel.find({$and:[{categories:{$in:[qCategory]}},{price:{$lte:price}},{color:{$regex:`${color}`,$options:"i"}}]})
+//     }
+//     else if(qCategory!==undefined && price!==undefined){
+//       products=await ProductModel.find({$and:[{categories:{$in:[qCategory]}},{price:{$lte:price}}]})
+//     }
+//     else if(qCategory!==undefined&&color!==undefined){
+//       products=await ProductModel.find({$and:[{categories:{$in:[qCategory]}},{color:{$regex:`${color}`,$options:"i"}}]})
+//     }
+//     else if(color!==undefined && price!==undefined){
+//       products=await ProductModel.find({$and:[{price:{$lte:price}},{color:{$regex:`${color}`,$options:"i"}}]})
+//     }
+//     else {
+//       if(qCategory!==undefined){
+//         products = await ProductModel.find({
+//             categories:{
+//               $in:[qCategory],
+//             },
+//         });
+//       }
+//       else if(color!==undefined){
+//         products=await ProductModel.find({color})
+//       }
+//       else if(price!==undefined){
+//         products=await ProductModel.find({price})
+//       }
+//       else {
+//         products=await ProductModel.find()
+//       }
+//     }
+    
+//    if(sort!==undefined){
+//     if(sort=='asc'){
+//       products.sort((a,b)=>a.price-b-price)
+//     }
+//     else{
+//       products.sort((a,b)=>b.price-a.price)
+//     }
+//    }
+//     products.length>0?res.status(200).send(products):res.status(200).send("There is no products")
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
 
 module.exports = {ProductsRoute};
