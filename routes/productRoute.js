e1377e97598997b9d6065c38a38c315d0369cf01
Sumit Pokhriyal <sumitstan1998@gmail.com>
2023-02-27 14:41:41 +0530

    product route get

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -61,15 +61,75 @@ ProductsRoute.get("/:id", async (req, res) => {
 //GET ALL ,Product, Anyone can access
 
 ProductsRoute.get("/", async (req, res) => {
-  const query = req.query;
-
+  const query=req.body
+  const qNew = req.query.new;
+  const qCategory = req.query.categories;
+  console.log({qCategory})
+  const price=req.query.price
+  const color=req.query.color
+  const sort=req.query.order
+  console.log({sort})
   try {
-   let products=await ProductModel.find(query);
-   res.status(200).send(products);
+    let products;
+   
+    if(price!==undefined &&color!==undefined && qCategory!==undefined){
+      products=await ProductModel.find({$and:[{categories:{$in:[qCategory]}},{price:{$lte:price}},{color:{$regex:`${color}`,$options:"i"}}]})
+    }
+    else if(qCategory!==undefined && price!==undefined){
+      products=await ProductModel.find({$and:[{categories:{$in:[qCategory]}},{price:{$lte:price}}]})
+    }
+    else if(qCategory!==undefined&&color!==undefined){
+      products=await ProductModel.find({$and:[{categories:{$in:[qCategory]}},{color:{$regex:`${color}`,$options:"i"}}]})
+    }
+    else if(color!==undefined && price!==undefined){
+      products=await ProductModel.find({$and:[{price:{$lte:price}},{color:{$regex:`${color}`,$options:"i"}}]})
+    }
+    else {
+      if(qCategory!==undefined){
+        products = await ProductModel.find({
+            categories:{
+              $in:[qCategory],
+            },
+        })
+      }
+      else if(color!==undefined){
+        products=await ProductModel.find({color})
+        products.reverse();
+      }
+      else if(price!==undefined){
+        products=await ProductModel.find({price})
+        products.reverse();
+      }
+      else{
+        products=await ProductModel.find()
+        products.reverse();
+      }
+    }
+    
+   if(sort!==undefined){
+    if(sort=='asc'){
+      products.sort((a,b)=>a.price-b-price)
+    }
+    else{
+      products.sort((a,b)=>b.price-a.price)
+    }
+   }
+    products.length>0?res.status(200).send(products):res.status(200).send("There is no products")
   } catch (err) {
     res.status(500).send(err);
   }
 });
+
+// ProductsRoute.get("/", async (req, res) => {
+//   const query = req.query;
+
+//   try {
+//    let products=await ProductModel.find(query);
+//    res.status(200).send(products);
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
 // ProductsRoute.get("/", async (req, res) => {
 //   const query=req.body
 //   const qNew = req.query.new;
